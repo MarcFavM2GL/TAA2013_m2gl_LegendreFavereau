955e0566f233bea7da9126b7b1267606d4e8d26d
MarcFavM2GL <favereau.marc.m2gl@gmail.com>
2013-10-11 08:34:10 +0200

    Vendredi matin !!!

--- src/main/java/ImplementationWebServices/PublishServicesImpl.java
+++ src/main/java/ImplementationWebServices/PublishServicesImpl.java
@@ -1,10 +1,15 @@
 package ImplementationWebServices;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 import interfacesWebServ.PublishServices;
 
+@Path("/publish")
 public class PublishServicesImpl implements PublishServices {
 
 	@Override
+	@GET
 	public String publishOnFaceBook() {
 		// TODO Auto-generated method stub
 		return "I am on FaceBook !!!";
@@ -12,6 +17,7 @@ public class PublishServicesImpl implements PublishServices {
 	}
 
 	@Override
+	@GET
 	public String publishOnTwitteroperation() {
 		// TODO Auto-generated method stub
 		return "Follow me on Twitter !!!";
